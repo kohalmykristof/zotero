5903dacdfdd4a93841375a5cc5b634e521d3c973
kohalmykristof <kohalmyk@gmail.com>
2017-02-14 18:19:34 +0100

    change https://vogon.feti.hu main url to https://mail.feti.hu/vogon/ change versions also in order to be able to sign add-on by mozilla (different version from previously released, signed one)

--- resource/config.js
+++ resource/config.js
@@ -8,14 +8,14 @@ var ZOTERO_CONFIG = {
 	BASE_URI: 'http://zotero.org/',
 	WWW_BASE_URL: 'https://www.zotero.org/',
 	PROXY_AUTH_URL: 'https://s3.amazonaws.com/zotero.org/proxy-auth',
-	SYNC_URL: 'https://vogon.feti.hu/sync/',
-	API_URL: 'https://vogon.feti.hu/',
+	SYNC_URL: 'https://mail.feti.hu/vogon/sync/',
+	API_URL: 'https://mail.feti.hu/vogon/',
 	API_VERSION: 2,
 	PREF_BRANCH: 'extensions.zotero.',
 	BOOKMARKLET_ORIGIN: 'https://www.zotero.org',
 	HTTP_BOOKMARKLET_ORIGIN: 'http://www.zotero.org',
 	BOOKMARKLET_URL: 'https://www.zotero.org/bookmarklet/',
-	VERSION: '4.0.29.17.SOURCE'
+	VERSION: '4.0.29.17.2.SOURCE'
 };
 
 EXPORTED_SYMBOLS = ["ZOTERO_CONFIG"];
